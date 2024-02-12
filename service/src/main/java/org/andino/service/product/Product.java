07209559469133eece52c3d3347ee866a12d8352
ganesan arunachalam <ganesan@andinolabs.com>
2024-02-12 16:06:39 +0530

    Enable check style

--- service/src/main/java/org/andino/service/product/Product.java
+++ service/src/main/java/org/andino/service/product/Product.java
@@ -1,10 +1,19 @@
 package org.andino.service.product;
 
-import jakarta.persistence.*;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Getter
 @Setter
@@ -16,7 +25,7 @@ public class Product {
 
 	@EmbeddedId
 	@AttributeOverride(name = "id", column = @Column(name = "id"))
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = IDENTITY)
 	private ProductId id;
 
 	@Embedded
--- service/src/test/java/org/andino/service/shared/PostgreSQLTestContainerExtension.java
+++ service/src/test/java/org/andino/service/shared/PostgreSQLTestContainerExtension.java
@@ -10,12 +10,12 @@ import static java.util.Collections.singletonMap;
 
 public class PostgreSQLTestContainerExtension implements BeforeAllCallback, AfterAllCallback {
 
-	private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(
+	private static final PostgreSQLContainer<?> POSTGRES_CONTAINER = new PostgreSQLContainer<>(
 			DockerImageName.parse("postgres:16.1").asCompatibleSubstituteFor(PostgreSQLContainer.IMAGE));
 
 	@Override
 	public void beforeAll(ExtensionContext context) {
-		System.out.println("PostgreSQL Container is Running: " + postgreSQLContainer.isRunning());
+		System.out.println("PostgreSQL Container is Running: " + POSTGRES_CONTAINER.isRunning());
 		startContainerIfNeed();
 	}
 
@@ -25,12 +25,12 @@ public class PostgreSQLTestContainerExtension implements BeforeAllCallback, Afte
 	}
 
 	private void startContainerIfNeed() {
-		if (!postgreSQLContainer.isRunning()) {
-			postgreSQLContainer.withEnv(singletonMap("ACCEPT_EULA", "Y")).withTmpFs(singletonMap("/testtmpfs", "rw"));
-			postgreSQLContainer.start();
-			System.setProperty("spring.datasource.url", postgreSQLContainer.getJdbcUrl());
-			System.setProperty("spring.datasource.username", postgreSQLContainer.getUsername());
-			System.setProperty("spring.datasource.password", postgreSQLContainer.getPassword());
+		if (!POSTGRES_CONTAINER.isRunning()) {
+			POSTGRES_CONTAINER.withEnv(singletonMap("ACCEPT_EULA", "Y")).withTmpFs(singletonMap("/testtmpfs", "rw"));
+			POSTGRES_CONTAINER.start();
+			System.setProperty("spring.datasource.url", POSTGRES_CONTAINER.getJdbcUrl());
+			System.setProperty("spring.datasource.username", POSTGRES_CONTAINER.getUsername());
+			System.setProperty("spring.datasource.password", POSTGRES_CONTAINER.getPassword());
 		}
 	}
 
